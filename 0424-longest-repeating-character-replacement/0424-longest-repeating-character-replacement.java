6dfd1c81199c5b1229fce369505842c9f6765feb
Rushikesh Tathe <rushikeshtathe870@gmail.com>
2026-07-31 15:28:55 +0530

    Time: 6 ms (98.81%), Space: 46.2 MB (51.1%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,49 +1,24 @@
 //it is similar to max consecutive one use use that code here to find freq of each char 
 //we initially find or store unique char in set along with checking feq of it and update
+
 class Solution {
     public int characterReplacement(String s, int k) {
         int n=s.length();
-        int longest=1;
-        HashSet <Character> set=new HashSet<>();
-
+        int frq[]=new int[26];
+         int maxLen = 0;
+        int left = 0;
+        int ans=0;
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
-            if(!set.contains(ch)){
-               int Substringlen= findfreq(ch,k,s);
-                longest=Math.max(longest,Substringlen);
-                set.add(ch);
-            }
+           frq[ch-'A']++;
+           maxLen = Math.max(maxLen,frq[ch-'A']);
+           while((i-left+1)-maxLen>k){
+            frq[s.charAt(left)-'A']--;
+            left++;
+           }
+           ans = Math.max(ans,i-left+1);
         }
-            return longest;
+            return ans;
 
-    }
-    private int findfreq(char ch ,int k,String s){
-        int n=s.length();
-         int maxLen = 0;
-        int right = 0;
-        int left = 0;
-
-        while (right < n) {
-
-            if (s.charAt(right) != ch && k > 0) {
-                k--;
-                right++;
-            }
-            else if (s.charAt(right) != ch && k == 0) {
-
-                while (s.charAt(left) == ch) {
-                    left++;
-                }
-                k++;
-                left++; // remove one zero from the window
-            }
-            else {
-                right++;
-            }
-
-            maxLen = Math.max(maxLen, right - left);
-        }
-
-        return maxLen;
-    }
-}
+    }}
+    
