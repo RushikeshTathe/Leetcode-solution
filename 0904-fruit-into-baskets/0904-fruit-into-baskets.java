01ae18d512f03fc0b0c407c958b335ee079af6d9
Rushikesh Tathe <rushikeshtathe870@gmail.com>
2026-08-15 20:17:49 +0530

    Time: 8 ms (95.18%), Space: 73.3 MB (5.16%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,22 +1,46 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         HashMap <Integer,Integer> map=new HashMap<>();
-        int n=fruits.length;
-        int maxFruits=0;
+        int  maxFruits=0;
+        int b1=-1;
+        int b2=-1;
+        int freqB1=0;
+        int freqB2=0;
+
         int left=0;
         int right=0;
-        while(right<n){
-            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
-
-            while(map.size()>2){
-                map.put(fruits[left],map.get(fruits[left])-1);
-                 if (map.get(fruits[left]) == 0) {
-                    map.remove(fruits[left]);
-                }
-                left++;
+        while(right<fruits.length){
+            if(fruits[right]==b1 || b1==-1){
+                b1=fruits[right];
+                freqB1++;
+                right++;
             }
-              maxFruits = Math.max(maxFruits, right - left + 1);
-            right++;
+             else if(fruits[right]==b2 || b2==-1){
+                b2=fruits[right];
+                  freqB2++;
+                right++;
+            }
+            else{
+                while((b1!=-1 && b2!=-1) && (freqB1>0 && freqB2>0)){
+                    if(fruits[left]==b1){
+                        freqB1--;
+                    }
+                    if(fruits[left]==b2){
+                        freqB2--;
+                    }
+                   
+                    left++;
+                }
+                if(freqB1==0){
+                    b1=fruits[right];
+                }
+                else if(freqB2==0){
+                    b2=fruits[right];
+                }
+                
+            }
+            maxFruits=Math.max(maxFruits,right-left);
+
         }
         return maxFruits;
     }
